b53b260e08b3fb96e67cd189eb8379c1c49e0e64
Mahmudur Rahman <hridoymahmud71@gmail.com>
2022-05-10 00:14:34 +0600

    readme update

--- README.md
+++ README.md
@@ -15,6 +15,24 @@ Go to https://testsafebrowsing.appspot.com/  to find malicious site links
 If the Original given url is empty/invalid/malicious , exeption(s) will be shown
 
 #### How to run
+Just run it like a laravel project by hitting url, not with serve
 _Vue was integrated using laravel mixin, so you will not be able to run the frontend using serve_
+import the link_ shortener.sql file (You may truncate the links table if you want to start fresh)
+
+#### Does this allow subfolder
+Yes, it does run on a subfolder
+in .env file if there is no subfolder , make sure the variables are like this
+
+BASENAME="/"
+MIX_ASSET_URL="/public"
+
+
+BASENAME="/link_shortener"
+MIX_ASSET_URL="/link_shortener/public"
+
+if you have subfolder the variables should be like:
+
+BASENAME="/[path/to/subfolder]"
+MIX_ASSET_URL="/[path/to/subfolder]/public"
 
 
--- resources/js/components/LinkGenarationComponent.vue
+++ resources/js/components/LinkGenarationComponent.vue
@@ -1,15 +1,20 @@
 <template>
     <div class=" bg-gray-200">
         <div class="container h-screen flex flex-col justify-center items-center">
+
+            <!-- show before link is generated -->
             <div class="flex justify-center items-center">                 
                 <input type="text" class="h-16 w-96 px-6  z-0 focus:shadow focus:outline-none" placeholder="Paste your link here">                    
                 <button class="h-16 p-4 rounded-r-md text-white  bg-blue-500 hover:bg-blue-600">Generate Link</button>                    
             </div>
+
+            <!-- show after link is generated -->
             <div class="flex justify-center items-center">                 
                 <input type="text" class="h-16 w-96 px-4 text-gray-600 text-2xl  z-0 focus:shadow focus:outline-none" value="https://192.158.63.65/23jds4e" readonly>                    
                 <button class="h-16 p-4 rounded-r-md text-white  bg-yellow-700 hover:bg-yellow-800">Copy Link</button>       
                 <label class="text-small text-gray-700 p-4">Copied</label>             
             </div>
+            <!-- response message -->
             <div class="mt-4">
                 <div class="text-green-600">This is a success response</div>
                 <div class="text-red-500">This is a Error Response</div>
